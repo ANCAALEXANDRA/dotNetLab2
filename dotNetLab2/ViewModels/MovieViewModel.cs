d9260506ad7c2c44d89ab557f41b42fdb8b790c8
Anca Avram <avram.anca.alexandra@gmail.com>
2021-05-15 09:10:34 +0300

    update MovieController

--- dotNetLab2/ViewModels/MovieViewModel.cs
+++ dotNetLab2/ViewModels/MovieViewModel.cs
@@ -2,24 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 
-namespace dotNetLab2.Models
+namespace dotNetLab2.ViewModels
 {
-    public class Movie
+    public class MovieViewModel
     {
         public long Id { get; set; }
-        [Required]
         public string Title { get; set; }
-        [Required]
         public string Description { get; set; }
-        //[Contains(new[] { "action", "thriller", "comedy", "horror" })]
         public string Gender { get; set; }
         public int DurationInMinutes { get; set; }
         public DateTime YearOfRelease { get; set; }
         public string Director { get; set; }
         public DateTime DateAdded { get; set; }
-        [Range(1, 10)]
         public int Rating { get; set; }
         public Boolean Watched { get; set; }
 
