ba10e09c311844b0ee02de6ddb56d71bcaad28ed
Anca Avram <avram.anca.alexandra@gmail.com>
2021-05-15 08:54:32 +0300

    update validations

---
+++ dotNetLab2/Migrations/20210515055143_UpdateMovie.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotNetLab2.Migrations
+{
+    public partial class UpdateMovie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Genre",
+                table: "Movies",
+                newName: "Gender");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Gender",
+                table: "Movies",
+                newName: "Genre");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- dotNetLab2/Models/Movie.cs
+++ dotNetLab2/Models/Movie.cs
@@ -2,22 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace dotNetLab2.Models
 {
     public class Movie
     {
         public long Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
-        public string Genre { get; set; }
+        //[Contains(new[] { "action", "thriller", "comedy", "horror" })]
+        public string Gender { get; set; }
         public int DurationInMinutes { get; set; }
         public DateTime YearOfRelease { get; set; }
         public string Director { get; set; }
         public DateTime DateAdded { get; set; }
+        [Range(1, 10)]
         public int Rating { get; set; }
         public bool Watched { get; set; }
-
 
     }
 }
